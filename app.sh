1bc67db5e4fb42ab4511001d2b7b77c3f0f8b0fa
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2022-12-16 09:35:33 +0800

    Add files via upload

---
+++ app.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#run EasyBot
+# notice: already "COPY app.json /etc/EasyBot/app.json" at Dockerfile
+/usr/local/bin/EasyBot -config=/etc/EasyBot/app.json &
+
+#run nginx
+# CMD ["nginx", "-g", "daemon off;"]
+nginx -g 'daemon off;'
+# /usr/local/nginx/sbin/nginx -g 'daemon off;'
+
+
+
